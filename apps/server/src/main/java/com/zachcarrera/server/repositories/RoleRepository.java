fc317af278cfdbabb1d87105a424f6237752e44a
Zach Carrera <zachcarrera@gmail.com>
2023-04-25 21:51:56 -0700

    create RoleRepository

---
+++ apps/server/src/main/java/com/zachcarrera/server/repositories/RoleRepository.java
@@ -0,0 +1,15 @@
+package com.zachcarrera.server.repositories;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.zachcarrera.server.models.Role;
+
+@Repository
+public interface RoleRepository extends CrudRepository<Role, Long>{
+    
+    List<Role> findAll();
+    
+}
