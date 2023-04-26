d03a321bfece0f60979c8eac41f8ff65c18a3338
Zach Carrera <zachcarrera@gmail.com>
2023-04-25 21:43:16 -0700

    create Home page

---
+++ apps/client/src/pages/Home/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Navbar } from "../../components";
+
+export const Home = () => {
+    return (
+        <>
+            <Navbar />
+            <div>Home</div>
+        </>
+    );
+};
