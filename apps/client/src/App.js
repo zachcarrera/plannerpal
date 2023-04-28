d3755d17754d1c811afec7683c8ff5eb9a06a552
Zach Carrera <zachcarrera@gmail.com>
2023-04-27 19:10:36 -0700

    add Routes for classes and progress and wrap entire app with QueryClientProvider

--- apps/client/src/App.js
+++ apps/client/src/App.js
@@ -1,17 +1,23 @@
 import { Route, Routes } from "react-router-dom";
-import { Home, NewAssignment } from "./pages";
+import { Classes, Home, NewAssignment, Progress } from "./pages";
 import { Navbar } from "./components";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
     return (
-        <div className="h-full bg-gray-100">
-            <Navbar />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<Home />} />
-                <Route path="/assignments/new" element={<NewAssignment />} />
-            </Routes>
-        </div>
+        <QueryClientProvider client={queryClient}>
+            <div className="h-screen bg-gray-100">
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/classes" element={<Classes />} />
+                    <Route path="/progress" element={<Progress />} />
+                    <Route path="/assignments/new" element={<NewAssignment />} />
+                </Routes>
+            </div>
+        </QueryClientProvider>
     );
 }
 
