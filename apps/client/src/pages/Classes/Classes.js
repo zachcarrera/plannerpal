fcaf1ed92185247d0a93f78111a693fa4484b81a
Zach Carrera <zachcarrera@gmail.com>
2023-04-27 17:55:34 -0700

    create Classes page

---
+++ apps/client/src/pages/Classes/Classes.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const Classes = () => {
+    return (
+        <div className="mx-auto my-2 w-1/2 rounded bg-white p-2 shadow">
+            <h1 className="text-center text-3xl">Classes</h1>
+
+            <div className="mx-auto w-3/4">
+                <h2 className="text-center text-2xl">Class 1</h2>
+                <ul>
+                    <li>Assignment 1</li>
+                    <li>Assignment 2</li>
+                    <li>Assignment 3</li>
+                </ul>
+            </div>
+        </div>
+    );
+};
