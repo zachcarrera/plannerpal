dd1a4945cf3261d0f950f6e335c44899095253a7
Zach Carrera <zachcarrera@gmail.com>
2023-04-27 20:05:09 -0700

    update Home page with styling for recent assignments

--- apps/client/src/pages/Home/Home.js
+++ apps/client/src/pages/Home/Home.js
@@ -1,11 +1,31 @@
 import React from "react";
-import { Navbar } from "../../components";
+import { Link } from "react-router-dom";
 
 export const Home = () => {
+    const recents = [1, 1, 1];
     return (
         <>
-            <Navbar />
-            <div>Home</div>
+            <div>
+                <h1 className="my-6 text-center text-4xl">
+                    Recent Assignments
+                </h1>
+                <div className="mx-auto flex items-center justify-center gap-8">
+                    {recents.map((num, index) => (
+                        <div
+                            key={index}
+                            className="rounded border bg-white p-10 shadow"
+                        >
+                            <h3 className="text-2xl">Assignment Title</h3>
+                            <Link
+                                to={`/assignments/${num}`}
+                                className="mt-3 block rounded-md bg-blue-600 px-1 py-2 text-center text-xl text-white hover:bg-blue-500"
+                            >
+                                View
+                            </Link>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </>
     );
 };
