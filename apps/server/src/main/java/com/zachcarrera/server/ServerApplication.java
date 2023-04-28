1056c7ab15c1a2754c408d50f0e0edcdaec01ea2
Zach Carrera <zachcarrera@gmail.com>
2023-04-27 20:20:48 -0700

    add test data for database

--- apps/server/src/main/java/com/zachcarrera/server/ServerApplication.java
+++ apps/server/src/main/java/com/zachcarrera/server/ServerApplication.java
@@ -1,5 +1,6 @@
 package com.zachcarrera.server;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.boot.ApplicationRunner;
@@ -8,8 +9,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import com.zachcarrera.server.models.Assignment;
+import com.zachcarrera.server.models.Course;
 import com.zachcarrera.server.models.Role;
 import com.zachcarrera.server.repositories.AssignmentRepository;
+import com.zachcarrera.server.repositories.CourseRepository;
 import com.zachcarrera.server.repositories.RoleRepository;
 
 @SpringBootApplication
@@ -20,10 +23,14 @@ public class ServerApplication {
 	}
 
 	@Bean
-	public ApplicationRunner applicationRunner (AssignmentRepository assignmentRepository, RoleRepository roleRepository) {
+	public ApplicationRunner applicationRunner (AssignmentRepository assignmentRepository, RoleRepository roleRepository, CourseRepository courseRepository) {
 		return args -> {
-			assignmentRepository.save(new Assignment("math"));
-			assignmentRepository.saveAll(List.of(new Assignment("gym"), new Assignment("science")));
+			// Course course1 = courseRepository.save(new Course("Physics"));
+			// Course course2 = courseRepository.save(new Course("Math"));
+			// assignmentRepository.save(new Assignment("Project Manager", new Date(1688972400000L), 3, course1));
+			// assignmentRepository.saveAll(List.of(new Assignment("CareSoft", new Date(1688972400000L), 4, course2), new Assignment("Book Club", new Date(1688972400000L), 2, course1)));
+			// assignmentRepository.save(new Assignment("Project Manager", new Date(1688972400000L), 3));
+			// assignmentRepository.saveAll(List.of(new Assignment("CareSoft", new Date(1688972400000L), 4), new Assignment("Book Club", new Date(1688972400000L), 2)));
 			roleRepository.saveAll(List.of(new Role("ROLE_STUDENT"), new Role("ROLE_INSTRUCTOR")));
 		};
 	}
