06380d015246e54d4f3bb8e5ae7fbc5716041988
Zach Carrera <zachcarrera@gmail.com>
2023-04-27 20:15:59 -0700

    create AssignmentService class

---
+++ apps/server/src/main/java/com/zachcarrera/server/services/AssignmentService.java
@@ -0,0 +1,48 @@
+package com.zachcarrera.server.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.zachcarrera.server.models.Assignment;
+import com.zachcarrera.server.repositories.AssignmentRepository;
+
+@Service
+public class AssignmentService {
+    @Autowired
+    private AssignmentRepository assignmentRepository;
+
+     // ----- FIND ALL -----
+    public List<Assignment> allAssignments() {
+        return assignmentRepository.findAll();
+    }
+
+    // ----- FIND ONE -----
+    public Assignment findAssignment(Long id) {
+        Optional<Assignment> optionalAssignment = assignmentRepository.findById(id);
+        if (optionalAssignment.isPresent()) {
+            return optionalAssignment.get();
+        }
+        return null;
+    }
+
+    // ----- CREATE -----
+    public Assignment createAssignment(Assignment newAssignment) {
+        return assignmentRepository.save(newAssignment);
+    }
+
+    // ----- UPDATE -----
+    public Assignment updateAssignment(Assignment oneAssignment) {
+        return assignmentRepository.save(oneAssignment);
+    }
+
+    // ----- DELETE -----
+    public void removeAssignment(Long id) {
+        assignmentRepository.deleteById(id);
+    }
+
+
+    
+}
