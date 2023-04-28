c59e7e43801f4e50d5e9135265a92cbbab808373
Zach Carrera <zachcarrera@gmail.com>
2023-04-27 21:50:04 -0700

    update Progress page to load courses from the backend

--- apps/client/src/pages/Progress/Progress.js
+++ apps/client/src/pages/Progress/Progress.js
@@ -1,11 +1,33 @@
 import React from "react";
 import { ProgressBar } from "../../components";
+import { getAllCourses } from "../../services";
+import { useQuery } from "@tanstack/react-query";
 
 export const Progress = () => {
+    const { data: classes, isLoading } = useQuery(["classes"], () =>
+        getAllCourses()
+    );
+
     return (
         <div className="mx-auto my-4 w-1/2 bg-white py-2">
-            <h1 className="text-center text-3xl">Progress</h1>
+            <h1 className="mb-4 text-center text-3xl">Progress</h1>
+
             {/* map over classes here */}
+            {classes?.map((oneClass) => (
+                <div key={oneClass.id} className="mx-auto my-2 w-3/4 ">
+                    <h2 className="text-center text-2xl">{oneClass.name}</h2>
+                    <ProgressBar
+                        percentage={
+                            Math.floor(
+                                oneClass.assignments.reduce(
+                                    (sum, assignment) => sum + 1,
+                                    0
+                                ) / oneClass.assignments.length
+                            ) * 100
+                        }
+                    />
+                </div>
+            ))}
             <div className="mx-auto my-2 w-3/4 ">
                 <h2 className="text-center text-2xl">Class 1</h2>
                 <ProgressBar percentage={20} />
