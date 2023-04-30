e62b75d2d7cf9fe900f4d698af49afcd0c1ba512
Zach Carrera <zachcarrera@gmail.com>
2023-04-30 08:43:10 -0700

    update pages/index.js with export for Login

--- apps/client/src/pages/index.js
+++ apps/client/src/pages/index.js
@@ -3,3 +3,4 @@ export { NewAssignment } from "./NewAssignment";
 export { Progress } from "./Progress";
 export { Classes } from "./Classes";
 export { Calendar } from "./Calendar";
+export { Login } from "./Login";
