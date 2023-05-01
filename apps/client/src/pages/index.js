151f3d4c3453e8f824350e92d266b25135c3c7c1
Zach Carrera <zachcarrera@gmail.com>
2023-04-30 23:51:29 -0700

    create Register page

--- apps/client/src/pages/index.js
+++ apps/client/src/pages/index.js
@@ -4,3 +4,4 @@ export { Progress } from "./Progress";
 export { Classes } from "./Classes";
 export { Calendar } from "./Calendar";
 export { Login } from "./Login";
+export { Register } from "./Register";
