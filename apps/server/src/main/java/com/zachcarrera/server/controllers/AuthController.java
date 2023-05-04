3e1c0b4542df752c96c7b6dc903c30890d64cf69
Zach Carrera <zachcarrera@gmail.com>
2023-05-04 14:32:35 -0700

    create test route at /api/auth

--- apps/server/src/main/java/com/zachcarrera/server/controllers/AuthController.java
+++ apps/server/src/main/java/com/zachcarrera/server/controllers/AuthController.java
@@ -1,11 +1,36 @@
 package com.zachcarrera.server.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.zachcarrera.server.models.User;
+import com.zachcarrera.server.repositories.UserRepository;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
 
+    @Autowired
+    private UserRepository userRepository;
+    
+
+    @GetMapping("")
+    public String test() {
+        return "hello from api";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "this is the login route";
+    }
+
+    @GetMapping("/users")
+    public List<User> users() {
+        return userRepository.findAll();
+    }
     
 }
