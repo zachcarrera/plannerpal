ef416a0e24d289dd2c8389e1f41a7c8c72f8d729
Zach Carrera <zachcarrera@gmail.com>
2023-05-05 12:03:48 -0700

    create services for logging in and getting all assignments, one assignment, or creating an assignment

--- apps/client/src/services/internalApiService.js
+++ apps/client/src/services/internalApiService.js
@@ -7,6 +7,14 @@ const http = axios.create({
     },
 });
 
+// ----- auth api -----
+export const loginUser = async (loginRequest) => {
+    const res = await http.post("/auth/login", loginRequest);
+    return res.data;
+};
+
+// ----- courses api -----
+
 export const getAllCourses = async () => {
     const res = await http.get("/courses");
     return res.data;
@@ -19,6 +27,18 @@ export const getOneCourse = async (id) => {
 
 export const createCourse = async (newCourse) => {
     const res = await http.post("/courses", newCourse);
+    return res.data;
+};
+
+// ----- assingments api -----
+
+export const getAllAssignments = async () => {
+    const res = await http.get("/assignments");
+    return res.data;
+};
+
+export const getOneAssignment = async (id) => {
+    const res = await http.get(`/assignments/${id}`);
     return res.data;
 };
 
