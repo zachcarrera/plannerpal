e2530db33b5c4a820faeeafdc7d88052e4a80752
Zach Carrera <zachcarrera@gmail.com>
2023-05-05 12:13:40 -0700

    create the CreateClassForm component

---
+++ apps/client/src/components/CreateClassForm/index.js
@@ -0,0 +1,1 @@
+export { CreateClassForm } from "./CreateClassForm";
--- apps/client/src/components/index.js
+++ apps/client/src/components/index.js
@@ -1,3 +1,4 @@
 export { Navbar } from "./Navbar";
 export { ProgressBar } from "./ProgressBar";
 export { LoadSpinner } from "./LoadSpinner";
+export { CreateClassForm } from "./CreateClassForm";
