bcb261d0c81717d59ea06c4c36b286fb3ba56312
Zach Carrera <zachcarrera@gmail.com>
2023-05-05 23:33:30 -0700

    update the login endpoint in AuthController to be a post mapping

--- apps/server/src/main/java/com/zachcarrera/server/controllers/AuthController.java
+++ apps/server/src/main/java/com/zachcarrera/server/controllers/AuthController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,7 +41,7 @@ public class AuthController {
         return "hello from api";
     }
 
-    @GetMapping("/login")
+    @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody AuthenticationRequest request) {
         authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
         User user = (User) userDetailsService.loadUserByUsername(request.getEmail());
