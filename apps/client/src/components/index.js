4f1937a8d9b1eab6e2f84ba7d9762bde5b5c3f9a
Zach Carrera <zachcarrera@gmail.com>
2023-05-08 22:10:37 -0700

    create RequireAuth component

--- apps/client/src/components/index.js
+++ apps/client/src/components/index.js
@@ -2,3 +2,4 @@ export { Navbar } from "./Navbar";
 export { ProgressBar } from "./ProgressBar";
 export { LoadSpinner } from "./LoadSpinner";
 export { CreateClassForm } from "./CreateClassForm";
+export { RequireAuth } from "./RequireAuth";
