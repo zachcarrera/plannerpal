485fd7261c32ba4d7d409c4c3bf454ba2455d642
Zach Carrera <zachcarrera@gmail.com>
2023-05-08 22:13:53 -0700

    create logic to protect routes to only be rendered if there is a user logged in

--- apps/client/src/components/RequireAuth/RequireAuth.js
+++ apps/client/src/components/RequireAuth/RequireAuth.js
@@ -1,5 +1,18 @@
 import React from "react";
+import { useAuth } from "../../hooks";
+import { useLocation, Outlet, Navigate } from "react-router-dom";
 
 export const RequireAuth = () => {
-    return <div>RequireAuth</div>;
+    const { auth } = useAuth();
+    const location = useLocation();
+    return (
+        <div>
+            {/* {true ? ( */}
+            {auth?.user ? (
+                <Outlet />
+            ) : (
+                <Navigate to="/login" state={{ from: location }} replace />
+            )}
+        </div>
+    );
 };
