0550530584903be4b3a6d0ec37b4fde07ef0a6f5
Zach Carrera <zachcarrera@gmail.com>
2023-05-09 16:18:22 -0700

    add CrossOrigin annotation to AuthController

--- apps/server/src/main/java/com/zachcarrera/server/controllers/AuthController.java
+++ apps/server/src/main/java/com/zachcarrera/server/controllers/AuthController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,6 +22,7 @@ import com.zachcarrera.server.repositories.UserRepository;
 import com.zachcarrera.server.services.UserDetailsServiceImpl;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/auth")
 public class AuthController {
 
