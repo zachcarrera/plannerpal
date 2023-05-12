218676efc3bbb4b2b25a01b36e97724f5f56b503
Zach Carrera <zachcarrera@gmail.com>
2023-05-12 14:25:26 -0700

    remove circular import of PasswordEncoder Bean in UserDetailsServiceImpl

--- apps/server/src/main/java/com/zachcarrera/server/services/UserDetailsServiceImpl.java
+++ apps/server/src/main/java/com/zachcarrera/server/services/UserDetailsServiceImpl.java
@@ -4,37 +4,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import com.zachcarrera.server.dto.RegisterRequest;
-import com.zachcarrera.server.models.User;
 import com.zachcarrera.server.repositories.UserRepository;
 
 @Service
-public class UserDetailsServiceImpl implements UserDetailsService{
+public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Autowired
     private UserRepository userRepository;
-
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         return userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("User not found"));
     }
 
-
-    public User registerUser(RegisterRequest request) {
-
-        User registeredUser = new User(request.getFirstName(), request.getLastName(), request.getEmail(), passwordEncoder.encode(request.getPassword()));
-
-        return userRepository.save(registeredUser);
-    }
-
-
-
-    
 }
