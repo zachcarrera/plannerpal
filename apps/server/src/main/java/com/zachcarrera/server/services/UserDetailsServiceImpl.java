2df9517a1f80c10ce3b4ebd71b623473e74f535d
Zach Carrera <zachcarrera@gmail.com>
2023-05-11 23:51:59 -0700

    update the registerUser method in the user service to save a User in the database with an encoded password

--- apps/server/src/main/java/com/zachcarrera/server/services/UserDetailsServiceImpl.java
+++ apps/server/src/main/java/com/zachcarrera/server/services/UserDetailsServiceImpl.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.zachcarrera.server.dto.RegisterRequest;
@@ -16,6 +17,10 @@ public class UserDetailsServiceImpl implements UserDetailsService{
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         return userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("User not found"));
@@ -23,7 +28,10 @@ public class UserDetailsServiceImpl implements UserDetailsService{
 
 
     public User registerUser(RegisterRequest request) {
-        return null;
+
+        User registeredUser = new User(request.getFirstName(), request.getLastName(), request.getEmail(), passwordEncoder.encode(request.getPassword()));
+
+        return userRepository.save(registeredUser);
     }
 
 
