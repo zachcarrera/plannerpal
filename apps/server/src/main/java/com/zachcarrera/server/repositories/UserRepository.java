5bfe2694dd6667eac18c18b9019d4c969a0c8783
Zach Carrera <zachcarrera@gmail.com>
2023-05-11 23:20:19 -0700

    add method in UserRepository to see if a user with a given email exists

--- apps/server/src/main/java/com/zachcarrera/server/repositories/UserRepository.java
+++ apps/server/src/main/java/com/zachcarrera/server/repositories/UserRepository.java
@@ -14,4 +14,6 @@ public interface UserRepository extends CrudRepository<User, Long> {
     List<User> findAll();
 
     Optional<User> findByEmail(String email);
+
+    Boolean existsByEmail(String email);
 }
