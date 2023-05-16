b575370eed2f17b54b1004778bfe0c6c95f65d83
Zach Carrera <zachcarrera@gmail.com>
2023-05-15 17:59:12 -0700

    persist courses and assignments in DatabaseInitializer

--- apps/server/src/main/java/com/zachcarrera/server/DatabaseInitializer.java
+++ apps/server/src/main/java/com/zachcarrera/server/DatabaseInitializer.java
@@ -1,6 +1,8 @@
 package com.zachcarrera.server;
 
+import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
@@ -8,6 +10,8 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import com.zachcarrera.server.models.Assignment;
+import com.zachcarrera.server.models.Course;
 import com.zachcarrera.server.models.Role;
 import com.zachcarrera.server.models.User;
 import com.zachcarrera.server.repositories.AssignmentRepository;
@@ -37,18 +41,22 @@ public class DatabaseInitializer implements ApplicationListener<ApplicationReady
     public void onApplicationEvent(ApplicationReadyEvent event) {
 
         System.out.println("first print");
-        // Course course1 = courseRepository.save(new Course("Physics"));
-        // Course course2 = courseRepository.save(new Course("Math"));
-        // assignmentRepository.save(new Assignment("Project Manager", new
-        // Date(1688972400000L), 3, course1));
-        // assignmentRepository.saveAll(List.of(new Assignment("CareSoft", new
-        // Date(1688972400000L), 4, course2), new Assignment("Book Club", new
-        // Date(1688972400000L), 2, course1)));
+
+        Course course1 = courseRepository.save(new Course("Physics"));
+        Course course2 = courseRepository.save(new Course("Math"));
+        assignmentRepository.save(new Assignment("Project Manager",
+                new Date(System.currentTimeMillis() + TimeUnit.DAYS.toMillis(5)), 3, course1));
+        assignmentRepository.saveAll(List.of(
+                new Assignment("CareSoft", new Date(System.currentTimeMillis() + TimeUnit.DAYS.toMillis(9)), 4,
+                        course2),
+                new Assignment("Book Club", new Date(System.currentTimeMillis() + TimeUnit.DAYS.toMillis(15)), 2,
+                        course1)));
         // assignmentRepository.save(new Assignment("Project Manager", new
         // Date(1688972400000L), 3));
         // assignmentRepository.saveAll(List.of(new Assignment("CareSoft", new
         // Date(1688972400000L), 4), new Assignment("Book Club", new
         // Date(1688972400000L), 2)));
+
         roleRepository.saveAll(List.of(new Role("ROLE_STUDENT"), new Role("ROLE_INSTRUCTOR")));
 
         userRepository.save(new User("Zach", "Carrera", "zach@mail.com", passwordEncoder.encode("password")));
