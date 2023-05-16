5885a251c37a104c6d042ceeffabc42436e476db
Zach Carrera <zachcarrera@gmail.com>
2023-05-15 23:33:51 -0700

    create styling and markup for OneAssignment with data from the backend

--- apps/client/src/pages/OneAssignment/OneAssignment.js
+++ apps/client/src/pages/OneAssignment/OneAssignment.js
@@ -1,5 +1,35 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
+import { useParams } from "react-router-dom";
+import { getOneAssignment } from "../../services/internalApiService";
+import { LoadSpinner } from "../../components";
 
 export const OneAssignment = () => {
-    return <div>OneAssignment</div>;
+    const { id } = useParams();
+    const { data: assignment, isLoading } = useQuery(["assignment"], () =>
+        getOneAssignment(id)
+    );
+
+    let dueDate;
+    if (!isLoading) {
+        dueDate = new Date(assignment.dueDate);
+        console.log(dueDate);
+    }
+    return (
+        <div className="mx-auto my-2 w-1/2 rounded bg-white p-2 shadow">
+            {isLoading ? (
+                <LoadSpinner />
+            ) : (
+                <>
+                    <h1 className="mb-4 text-center text-3xl">
+                        {assignment.name}
+                    </h1>
+                    <p className="text-center">
+                        <span className="font-semibold">Due Date: </span>
+                        {dueDate.toLocaleDateString("en-us")}
+                    </p>
+                </>
+            )}
+        </div>
+    );
 };
