238574b49fccfbf780bb371e3e0fa25609b7005c
Zach Carrera <zachcarrera@gmail.com>
2023-05-16 18:11:43 -0700

    update AssignmentService to throw an AssignnentNotFoundException in findAssignment

--- apps/server/src/main/java/com/zachcarrera/server/services/AssignmentService.java
+++ apps/server/src/main/java/com/zachcarrera/server/services/AssignmentService.java
@@ -1,11 +1,11 @@
 package com.zachcarrera.server.services;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.zachcarrera.server.exception.AssignmentNotFoundException;
 import com.zachcarrera.server.models.Assignment;
 import com.zachcarrera.server.repositories.AssignmentRepository;
 
@@ -21,11 +21,8 @@ public class AssignmentService {
 
     // ----- FIND ONE -----
     public Assignment findAssignment(Long id) {
-        Optional<Assignment> optionalAssignment = assignmentRepository.findById(id);
-        if (optionalAssignment.isPresent()) {
-            return optionalAssignment.get();
-        }
-        return null;
+        return assignmentRepository.findById(id)
+                .orElseThrow(() -> new AssignmentNotFoundException("Assignment not found with id: " + id));
     }
 
     // ----- CREATE -----
