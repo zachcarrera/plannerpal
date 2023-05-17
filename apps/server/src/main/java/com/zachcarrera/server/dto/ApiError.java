e8165af55a34ea3aa3dde32c5c2488fa8001c567
Zach Carrera <zachcarrera@gmail.com>
2023-05-16 18:07:05 -0700

    create ApiError dto

---
+++ apps/server/src/main/java/com/zachcarrera/server/dto/ApiError.java
@@ -0,0 +1,42 @@
+package com.zachcarrera.server.dto;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+
+public class ApiError {
+
+    private String message;
+    private HttpStatus status;
+    private Date timestamp;
+
+    public ApiError(String message, HttpStatus status, Date timestamp) {
+        this.message = message;
+        this.status = status;
+        this.timestamp = timestamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(HttpStatus status) {
+        this.status = status;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+}
