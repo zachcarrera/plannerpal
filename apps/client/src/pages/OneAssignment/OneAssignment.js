7f40f3b9b1504297c47597779d3a42bf60b955da
Zach Carrera <zachcarrera@gmail.com>
2023-05-17 23:18:42 -0700

    add a Badge component to the OneAssignment page with the variant and text of the Badge being determined by the query from the backend

--- apps/client/src/pages/OneAssignment/OneAssignment.js
+++ apps/client/src/pages/OneAssignment/OneAssignment.js
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import { useParams } from "react-router-dom";
 import { getOneAssignment } from "../../services/internalApiService";
-import { LoadSpinner } from "../../components";
+import { Badge, LoadSpinner } from "../../components";
 
 export const OneAssignment = () => {
     const { id } = useParams();
@@ -11,8 +11,16 @@ export const OneAssignment = () => {
     );
 
     let dueDate;
+    const badge = {};
     if (!isLoading) {
         dueDate = new Date(assignment.dueDate);
+        if (assignment.completed) {
+            badge.variant = "green";
+            badge.text = "Completed";
+        } else {
+            badge.variant = "gray";
+            badge.text = "Not Completed";
+        }
     }
     return (
         <div className="mx-auto my-2 w-1/2 rounded bg-white p-2 shadow">
@@ -23,10 +31,13 @@ export const OneAssignment = () => {
                     <h1 className="mb-4 text-center text-3xl">
                         {assignment.name}
                     </h1>
-                    <p className="text-center">
-                        <span className="font-semibold">Due Date: </span>
-                        {dueDate.toLocaleDateString("en-us")}
-                    </p>
+                    <div className="flex justify-center gap-4">
+                        <p className="text-center">
+                            <span className="font-semibold">Due Date: </span>
+                            {dueDate.toLocaleDateString("en-us")}
+                        </p>
+                        <Badge variant={badge.variant}>{badge.text}</Badge>
+                    </div>
                 </>
             )}
         </div>
