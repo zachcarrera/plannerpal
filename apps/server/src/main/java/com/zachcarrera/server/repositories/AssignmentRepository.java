56302d8ea4df905966a3374f413b22d927f1a094
Zach Carrera <zachcarrera@gmail.com>
2023-05-17 23:44:04 -0700

    add a jpa query in the AssignmentRepository to find assignments by User

--- apps/server/src/main/java/com/zachcarrera/server/repositories/AssignmentRepository.java
+++ apps/server/src/main/java/com/zachcarrera/server/repositories/AssignmentRepository.java
@@ -7,11 +7,14 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.zachcarrera.server.models.Assignment;
+import com.zachcarrera.server.models.User;
 
 @Repository
 public interface AssignmentRepository extends CrudRepository<Assignment, Long> {
 
     List<Assignment> findAll();
+
+    List<Assignment> findByUser(User user);
 
     List<Assignment> findTop3ByOrderByCreatedAtDesc();
 
