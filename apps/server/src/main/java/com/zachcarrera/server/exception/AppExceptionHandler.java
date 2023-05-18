145a3a9219916c671b2b1f1e69059af4cd6dd65e
Zach Carrera <zachcarrera@gmail.com>
2023-05-17 23:55:01 -0700

    update AppExceptionHandler to use the new ApiError constructor

--- apps/server/src/main/java/com/zachcarrera/server/exception/AppExceptionHandler.java
+++ apps/server/src/main/java/com/zachcarrera/server/exception/AppExceptionHandler.java
@@ -1,7 +1,5 @@
 package com.zachcarrera.server.exception;
 
-import java.util.Date;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -16,7 +14,7 @@ public class AppExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(AssignmentNotFoundException.class)
     public ResponseEntity<ApiError> handleAssignmentNotFound(AssignmentNotFoundException ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(new ApiError(ex.getMessage(), HttpStatus.NOT_FOUND, new Date()));
+                .body(new ApiError(ex.getMessage(), HttpStatus.NOT_FOUND));
         // return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage() + new
         // Date());
     }
