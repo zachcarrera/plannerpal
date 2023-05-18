35ca5e70aa79348ef2a6a2a8e38c4820b4703ae5
Zach Carrera <zachcarrera@gmail.com>
2023-05-17 23:32:40 -0700

    update the Home page to use the getTop3Assignments service and populate the page with the data from that query

--- apps/client/src/pages/Home/Home.js
+++ apps/client/src/pages/Home/Home.js
@@ -1,23 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { getTop3Assignments } from "../../services";
+import { useQuery } from "@tanstack/react-query";
 
 export const Home = () => {
-    const recents = [1, 1, 1];
+    const { data: assignments, isLoading } = useQuery(
+        ["assignments", "top3"],
+        () => getTop3Assignments()
+    );
     return (
         <>
             <div>
                 <h1 className="my-6 text-center text-4xl">
                     Recent Assignments
                 </h1>
-                <div className="mx-auto flex items-center justify-center gap-8">
-                    {recents.map((num, index) => (
+                <div className="mx-auto flex w-2/3 items-center justify-center gap-8">
+                    {assignments?.map((assignment) => (
                         <div
-                            key={index}
-                            className="rounded border bg-white p-10 shadow"
+                            key={assignment.id}
+                            className="w-full rounded border bg-white p-10 shadow"
                         >
-                            <h3 className="text-2xl">Assignment Title</h3>
+                            <h3 className="text-center text-2xl">
+                                {assignment.name}
+                            </h3>
                             <Link
-                                to={`/assignments/${num}`}
+                                to={`/assignments/${assignment.id}`}
                                 className="mt-3 block rounded-md bg-blue-600 px-1 py-2 text-center text-xl text-white hover:bg-blue-500"
                             >
                                 View
