6df05ef9b79e4fe6a3466be024dd62974776a2fd
Zach Carrera <zachcarrera@gmail.com>
2023-05-17 22:52:39 -0700

    create default and variant styling for the Badge component

--- apps/client/src/components/Badge/Badge.js
+++ apps/client/src/components/Badge/Badge.js
@@ -1,5 +1,20 @@
 import React from "react";
 
-export const Badge = () => {
-    return <div>Badge</div>;
+const colors = {
+    gray: "bg-gray-100 text-gray-800",
+    red: "bg-red-100 text-red-800",
+    blue: "bg-blue-100 text-blue-800",
+    green: "bg-green-100 text-green-800",
+};
+
+export const Badge = ({ children, variant }) => {
+    if (!variant) variant = "gray";
+
+    return (
+        <span
+            className={`rounded px-2.5 py-0.5 text-sm font-medium  ${colors[variant]}`}
+        >
+            {children}
+        </span>
+    );
 };
